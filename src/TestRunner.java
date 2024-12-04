348c96fd3d305614d9c58a296cdf8bfb9587db76
xnnixa <154546393+xnnixa@users.noreply.github.com>
2024-12-04 12:07:49 +0200

    tests added

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.notification.Failure;
 
 public class TestRunner {
    public static void main(String[] args) {
-      Result result = JUnitCore.runClasses(SudokuVerifierParameterizedTest.class);
+      Result result = JUnitCore.runClasses(SudokuVerifierTest.class);
 
       for (Failure failure : result.getFailures()) {
          System.out.println(failure.toString());
--- tests/SudokuVerifierTest.java
+++ tests/SudokuVerifierTest.java
@@ -10,6 +10,11 @@ public class SudokuVerifierTest {
 // An incorrect Sudoku string: 123456789912345678891234567789123456678912345567891234456789123345678912234567891
 String c = "417369825632158947958724316825437169791586432346912758289643571573291684164875293";
 String i = "123456789912345678891234567789123456678912345567891234456789123345678912234567891";
+String s = "a17369825632158947958724316825437169791586432346912758289643571573291684164875293";
+String d = "9417369825632158947958724316825437169791586432346912758289643571573291684164875293";
+String f = "17369825632158947958724316825437169791586432346912758289643571573291684164875293";
+String g = "41736982563215894795872431682543716979158643234691275828964357157329168416487529!";
+String h = "017369825632158947958724316825437169791586432346912758289643571573291684164875293";
 SudokuVerifier v = new SudokuVerifier();
 
 	@Test
@@ -25,5 +30,39 @@ SudokuVerifier v = new SudokuVerifier();
 		
 	}
 	
-	
+	@Test
+	public void testStringWithLetter () {
+		int a = v.verify(s);
+		assertEquals("incorrect string", a, 1);
+	}
+
+	@Test
+	public void testStringTooLong () {
+		int a = v.verify(d);
+		assertEquals("incorrect string", a, -1);
+	}
+
+	@Test
+	public void testStringTooShort () {
+		int a = v.verify(f);
+		assertEquals("incorrect string", a, -1);
+	}
+
+	@Test
+	public void testHasSpecialCharacter () {
+		int a = v.verify(g);
+		assertEquals("incorrect string", a, 1);
+	}
+
+	@Test
+	public void testEmptyString () {
+		int a = v.verify("");
+		assertEquals("incorrect string", a, -1);
+	}
+
+	@Test 
+	public void testStringHasZero () {
+		int a = v.verify(h);
+		assertEquals("incorrect string", a, -1);
+	}
 }
